5dc948e0331ab8462f5445d71993efecb3036e05
Yiwei Gao <ygao93@gmail.com>
2015-03-03 18:37:31 -0500

    added media player, buttons, and hooked up most of the buttons to go with the media player

--- app/src/main/java/com/yiweigao/campustours/ControlPanelFragment.java
+++ app/src/main/java/com/yiweigao/campustours/ControlPanelFragment.java
@@ -1,21 +1,76 @@
 package com.yiweigao.campustours;
 
 import android.app.Fragment;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by yiweigao on 2/26/15.
  */
 public class ControlPanelFragment extends Fragment {
+
+    View mInflatedView;
+    MediaPlayer mMediaPlayer = null;
+    Button mPrevButton;
+    Button mPlayButton;
+    Button mNextButton;
+    boolean mIsPlaying = false;
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
 
-        return inflater.inflate(R.layout.control_panel, container, false);
+        mInflatedView = inflater.inflate(R.layout.control_panel_fragment, container, false);
+
+        mMediaPlayer = MediaPlayer.create(getActivity(), R.raw.emory_university_overview);
+
+        mPrevButton = (Button) mInflatedView.findViewById(R.id.control_panel_prev_button);
+        mPlayButton = (Button) mInflatedView.findViewById(R.id.control_panel_play_button);
+        mNextButton = (Button) mInflatedView.findViewById(R.id.control_panel_next_button);
+        
+        
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mMediaPlayer.isPlaying()) {
+                    int currentPosition = mMediaPlayer.getCurrentPosition();
+                    mMediaPlayer.seekTo(currentPosition - 10000);
+//                    mMediaPlayer.start();
+                }
+
+            }
+        });
+        
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.start();
+                    mPlayButton.setText(R.string.control_panel_pause_button_text);
+                } else if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                    mPlayButton.setText(R.string.control_panel_play_button_text);
+                }
+
+            }
+        });
+        
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        
+        return mInflatedView;
+        
     }
 }

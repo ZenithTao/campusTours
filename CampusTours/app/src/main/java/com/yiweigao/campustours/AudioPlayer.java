f23326dd784f9fbe9689632849411369d193e991
Yiwei Gao <ygao93@gmail.com>
2015-04-06 17:10:43 -0400

    created audioplayer class

--- CampusTours/app/src/main/java/com/yiweigao/campustours/AudioPlayer.java
+++ CampusTours/app/src/main/java/com/yiweigao/campustours/AudioPlayer.java
@@ -1,8 +1,26 @@
 package com.yiweigao.campustours;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 /**
  * Created by yiweigao on 4/6/15.
  */
 
 public class AudioPlayer {
+
+    private MediaPlayer mPlayer;
+    
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+    
+    public void play(Context context) {
+        mPlayer = MediaPlayer.create(context, R.raw.emory_university_overview);
+        mPlayer.start();
+    }
+    
 }

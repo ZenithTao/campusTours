1bfaa6e1a0ef83e469d2916141c2a71f708be8a9
Yiwei Gao <ygao93@gmail.com>
2015-04-16 13:05:59 -0400

    added Tutorial class to handle SCV

---
+++ CampusTours/app/src/main/java/com/yiweigao/campustours/Tutorial.java
@@ -0,0 +1,86 @@
+package com.yiweigao.campustours;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Point;
+
+import com.github.amlcurran.showcaseview.OnShowcaseEventListener;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.PointTarget;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
+
+/**
+ * Created by yiweigao on 4/16/15.
+ */
+
+public class Tutorial {
+
+    private Context mContext;
+
+    public Tutorial(Context context) {
+        mContext = context;
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences("com.yiweigao.CampusTours", Context.MODE_PRIVATE);
+        if (sharedPreferences.getBoolean("first_run", true)) {
+            sharedPreferences.edit().putBoolean("first_run", false).apply();
+            launchMapShowCaseView();
+        }
+    }
+
+    private void launchMapShowCaseView() {
+        Point screenSize = new Point();
+        ((Activity) mContext).getWindowManager().getDefaultDisplay().getSize(screenSize);
+        new ShowcaseView.Builder((Activity) mContext, true)
+                .setTarget(new PointTarget(new Point(screenSize.x, 0)))
+                .setStyle(R.style.CustomShowcaseTheme)
+                .setContentTitle("Using the map")
+                .setContentText("Make sure that your GPS is turned on, and click on this button to show your current location on the map.\n\nTap anywhere to dismiss this message")
+                .setShowcaseEventListener(new OnShowcaseEventListener() {
+                    @Override
+                    public void onShowcaseViewHide(ShowcaseView showcaseView) {
+
+                    }
+
+                    @Override
+                    public void onShowcaseViewDidHide(ShowcaseView showcaseView) {
+                        launchControlPanelShowCaseView();
+                    }
+
+                    @Override
+                    public void onShowcaseViewShow(ShowcaseView showcaseView) {
+                        showcaseView.hideButton();
+                    }
+                })
+                .hideOnTouchOutside()
+                .build();
+    }
+
+    private void launchControlPanelShowCaseView() {
+
+        ViewTarget viewTarget = new ViewTarget(R.id.control_panel_play_button, (Activity) mContext);
+        new ShowcaseView.Builder((Activity) mContext, true)
+                .setTarget(viewTarget)
+                .setStyle(R.style.CustomShowcaseTheme)
+                .setContentTitle("Using the audio controls")
+                .setContentText("Audio clips will play automatically along the tour, but you can use these buttons to rewind, play/pause, and fast forward at your leisure." +
+                        "\n\nEnjoy your tour!")
+                .setShowcaseEventListener(new OnShowcaseEventListener() {
+                    @Override
+                    public void onShowcaseViewHide(ShowcaseView showcaseView) {
+
+                    }
+
+                    @Override
+                    public void onShowcaseViewDidHide(ShowcaseView showcaseView) {
+
+                    }
+
+                    @Override
+                    public void onShowcaseViewShow(ShowcaseView showcaseView) {
+                        showcaseView.hideButton();
+                    }
+                })
+                .hideOnTouchOutside()
+                .build();
+    }
+}

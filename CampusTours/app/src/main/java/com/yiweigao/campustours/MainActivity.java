dd405e0192746c7b54316e4f24eeabe6289c9eaf
Yiwei Gao <ygao93@gmail.com>
2015-04-15 20:08:46 -0400

    all map and location services code has been migrated to MapManager class

--- CampusTours/app/src/main/java/com/yiweigao/campustours/MainActivity.java
+++ CampusTours/app/src/main/java/com/yiweigao/campustours/MainActivity.java
@@ -4,31 +4,26 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.PendingIntent;
-import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
-import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.MapFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
+//        GoogleApiClient.ConnectionCallbacks,
+//        GoogleApiClient.OnConnectionFailedListener,
 //        OnMapReadyCallback,
         LocationListener,
         ResultCallback<Status> {
@@ -63,38 +58,37 @@ public class MainActivity extends ActionBarActivity implements
             if (mMapFragment == null) {
                 mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
                 mMapManager = new MapManager(this, mMapFragment);
-                mMapManager.getMap();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        buildGoogleApiClient();
+//        buildGoogleApiClient();
 
-        populateGeofenceList();
+//        populateGeofenceList();
 
-        createLocationRequest();
+//        createLocationRequest();
 
     }
 
-    protected void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
+//    protected void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API)
+//                .build();
+//    }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+//        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mGoogleApiClient.disconnect();
+//        mGoogleApiClient.disconnect();
     }
 
 //    @Override
@@ -124,28 +118,28 @@ public class MainActivity extends ActionBarActivity implements
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        LocationServices.GeofencingApi.addGeofences(
-                mGoogleApiClient,
-                getGeofencingRequest(),
-                getGeofencePendingIntent()
-        ).setResultCallback(this);
-
-        startLocationUpdates();
-    }
+//    @Override
+//    public void onConnected(Bundle bundle) {
+//        LocationServices.GeofencingApi.addGeofences(
+//                mGoogleApiClient,
+//                getGeofencingRequest(),
+//                getGeofencePendingIntent()
+//        ).setResultCallback(this);
+//
+//        startLocationUpdates();
+//    }
 
     private void startLocationUpdates() {
         LocationServices.FusedLocationApi.requestLocationUpdates(
                 mGoogleApiClient, mLocationRequest, this);
     }
 
-    private void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
+//    private void createLocationRequest() {
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(10000);
+//        mLocationRequest.setFastestInterval(5000);
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//    }
 
     @Override
     public void onLocationChanged(Location location) {
@@ -156,20 +150,20 @@ public class MainActivity extends ActionBarActivity implements
      * Builds and returns a GeofencingRequest. Specifies the list of geofences to be monitored.
      * Also specifies how the geofence notifications are initially triggered.
      */
-    private GeofencingRequest getGeofencingRequest() {
-        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-
-        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
-        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
-        // is already inside that geofence.
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
-
-        // Add the geofences to be monitored by geofencing service.
-        builder.addGeofences(mGeofenceList);
-
-        // Return a GeofencingRequest.
-        return builder.build();
-    }
+//    private GeofencingRequest getGeofencingRequest() {
+//        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+//
+//        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+//        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+//        // is already inside that geofence.
+//        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+//
+//        // Add the geofences to be monitored by geofencing service.
+//        builder.addGeofences(mGeofenceList);
+//
+//        // Return a GeofencingRequest.
+//        return builder.build();
+//    }
 
     /**
      * Gets a PendingIntent to send with the request to add or remove Geofences. Location Services
@@ -178,71 +172,71 @@ public class MainActivity extends ActionBarActivity implements
      *
      * @return A PendingIntent for the IntentService that handles geofence transitions.
      */
-    private PendingIntent getGeofencePendingIntent() {
-        // Reuse the PendingIntent if we already have it.
-        if (mGeofencePendingIntent != null) {
-            return mGeofencePendingIntent;
-        }
-        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
-        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
-        // addGeofences() and removeGeofences().
-        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
+//    private PendingIntent getGeofencePendingIntent() {
+//        // Reuse the PendingIntent if we already have it.
+//        if (mGeofencePendingIntent != null) {
+//            return mGeofencePendingIntent;
+//        }
+//        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+//        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+//        // addGeofences() and removeGeofences().
+//        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//    }
 
-    public void populateGeofenceList() {
-        mGeofenceList = new ArrayList<Geofence>();
-        mGeofenceList.add(new Geofence.Builder()
-                .setRequestId(DebugResource.HOUSE_TEXT)
-                .setCircularRegion(
-                        DebugResource.HOUSE.latitude,
-                        DebugResource.HOUSE.longitude,
-                        DebugResource.HOUSE_RADIUS)
-                .setExpirationDuration(DebugResource.HOUSE_LIFETIME)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
-                        Geofence.GEOFENCE_TRANSITION_EXIT)
-                .build());
+//    public void populateGeofenceList() {
+//        mGeofenceList = new ArrayList<Geofence>();
+//        mGeofenceList.add(new Geofence.Builder()
+//                .setRequestId(DebugResource.HOUSE_TEXT)
+//                .setCircularRegion(
+//                        DebugResource.HOUSE.latitude,
+//                        DebugResource.HOUSE.longitude,
+//                        DebugResource.HOUSE_RADIUS)
+//                .setExpirationDuration(DebugResource.HOUSE_LIFETIME)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+//                        Geofence.GEOFENCE_TRANSITION_EXIT)
+//                .build());
+//
+//        mGeofenceList.add(new Geofence.Builder()
+//                .setRequestId(DebugResource.TEST_ONE)
+//                .setCircularRegion(
+//                        DebugResource.TEST_ONE_LATLNG.latitude,
+//                        DebugResource.TEST_ONE_LATLNG.longitude,
+//                        DebugResource.TEST_ONE_RADIUS)
+//                .setExpirationDuration(DebugResource.TEST_ONE_LIFETIME)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+//                .build());
+//
+//        mGeofenceList.add(new Geofence.Builder()
+//                .setRequestId(DebugResource.TEST_TWO)
+//                .setCircularRegion(
+//                        DebugResource.TEST_TWO_LATLNG.latitude,
+//                        DebugResource.TEST_TWO_LATLNG.longitude,
+//                        DebugResource.TEST_TWO_RADIUS)
+//                .setExpirationDuration(DebugResource.TEST_TWO_LIFETIME)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+//                .build());
+//
+//        mGeofenceList.add(new Geofence.Builder()
+//                .setRequestId(DebugResource.TEST_THREE)
+//                .setCircularRegion(
+//                        DebugResource.TEST_THREE_LATLNG.latitude,
+//                        DebugResource.TEST_THREE_LATLNG.longitude,
+//                        DebugResource.TEST_THREE_RADIUS)
+//                .setExpirationDuration(DebugResource.TEST_THREE_LIFETIME)
+//                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_EXIT)
+//                .build());
+//
+//    }
 
-        mGeofenceList.add(new Geofence.Builder()
-                .setRequestId(DebugResource.TEST_ONE)
-                .setCircularRegion(
-                        DebugResource.TEST_ONE_LATLNG.latitude,
-                        DebugResource.TEST_ONE_LATLNG.longitude,
-                        DebugResource.TEST_ONE_RADIUS)
-                .setExpirationDuration(DebugResource.TEST_ONE_LIFETIME)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-                .build());
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//        mGoogleApiClient.connect();
+//    }
 
-        mGeofenceList.add(new Geofence.Builder()
-                .setRequestId(DebugResource.TEST_TWO)
-                .setCircularRegion(
-                        DebugResource.TEST_TWO_LATLNG.latitude,
-                        DebugResource.TEST_TWO_LATLNG.longitude,
-                        DebugResource.TEST_TWO_RADIUS)
-                .setExpirationDuration(DebugResource.TEST_TWO_LIFETIME)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-                .build());
-
-        mGeofenceList.add(new Geofence.Builder()
-                .setRequestId(DebugResource.TEST_THREE)
-                .setCircularRegion(
-                        DebugResource.TEST_THREE_LATLNG.latitude,
-                        DebugResource.TEST_THREE_LATLNG.longitude,
-                        DebugResource.TEST_THREE_RADIUS)
-                .setExpirationDuration(DebugResource.TEST_THREE_LIFETIME)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_EXIT)
-                .build());
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(this, "Connection failed!", Toast.LENGTH_LONG).show();
-    }
+//    @Override
+//    public void onConnectionFailed(ConnectionResult connectionResult) {
+//        Toast.makeText(this, "Connection failed!", Toast.LENGTH_LONG).show();
+//    }
 
     /**
      * Runs when the result of calling addGeofences() and removeGeofences() becomes available.
--- CampusTours/app/src/main/java/com/yiweigao/campustours/MapManager.java
+++ CampusTours/app/src/main/java/com/yiweigao/campustours/MapManager.java
@@ -1,13 +1,25 @@
 package com.yiweigao.campustours;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -37,35 +49,60 @@ import java.util.List;
  */
 
 // manages all map activity (location, gps connection, map drawings, etc)
-public class MapManager implements OnMapReadyCallback {
+public class MapManager implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<Status>,
+        LocationListener {
+
 
     // some constants
     private static final LatLng TOUR_START = new LatLng(33.789591, -84.326506);
     private static final String BASE_URL = "http://dutch.mathcs.emory.edu:8009/";
-
-    // context for making toasts
+    private static final int GEOFENCE_LIFETIME = 100000;
+    List<GeofenceObject> listOfGeofenceObjects = new ArrayList<>();
+    List<Geofence> listOfGeofences = new ArrayList<>();
+    // context for making toasts and generating intents
     private Context mContext;
-    
-    // 
+    // map related
     private MapFragment mMapFragment;
     private GoogleMap mGoogleMap;
-    
+    // location related
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private PendingIntent mGeofencePendingIntent = null;
     private Toast loadingToast;
 
-    private List<LatLng> mRouteCoordinates = new ArrayList<>();
+//    private List<LatLng> mRouteCoordinates = new ArrayList<>();
 //    private List<GeofenceObject> mGeofenceCoordinates = new ArrayList<>();
 
     public MapManager(Context context, MapFragment mapFragment) {
         this.mContext = context;
         this.mMapFragment = mapFragment;
+        getMap();
+        createLocationRequest();
+        buildGoogleApiClient();
+//        mGoogleApiClient.connect();
     }
 
-    public MapManager(Context context, GoogleMap googleMap) {
-        mContext = context;
-        mGoogleMap = googleMap;
-        setInitialView();
-        getRoute();
-        getGeofences();
+//    public MapManager(Context context, GoogleMap googleMap) {
+//        mContext = context;
+//        mGoogleMap = googleMap;
+//        setInitialView();
+//        getRoute();
+//        getGeofences();
+//        buildGoogleApiClient();
+//        createLocationRequest();
+//    }
+
+    protected void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+//        Log.d("build client", "---was just called---");
     }
 
     private void setInitialView() {
@@ -86,33 +123,21 @@ public class MapManager implements OnMapReadyCallback {
     }
 
     private void getRoute() {
-        // executes a new AsyncTask to fetch coordinates from API
         new DownloadRouteTask().execute(BASE_URL + "points");
     }
 
-    private void getGeofences() {
-        new DownloadGeofencesTask().execute(BASE_URL + "geofences");
-    }
-
-    // draws route using stored coordinates
-    private void drawRoute() {
+    private void drawRoute(List<LatLng> listOfRouteCoordinates) {
         mGoogleMap.addPolyline(new PolylineOptions()
                 .color(Color.argb(255, 0, 40, 120))     // emory blue, 100% opacity
 //                .color(Color.argb(255, 210, 176, 0))    // emory "web light gold", 100% opacity
 //                .color(Color.argb(255, 210, 142, 0))    // emory "web dark gold", 100% opacity
                 .geodesic(true)
-                .addAll(mRouteCoordinates));
+                .addAll(listOfRouteCoordinates));
     }
 
-//    private void drawGeofences() {
-//
-//        for (GeofenceObject geofenceObject : mGeofenceCoordinates) {
-//            mGoogleMap.addCircle(new CircleOptions()
-//                    .center(geofenceObject.getCoordinates())
-//                    .radius(geofenceObject.getRadius())
-//                    .strokeWidth(5.0f));      // width in pixels, default = 10
-//        }
-//    }
+    private void getGeofences() {
+        new DownloadGeofencesTask().execute(BASE_URL + "geofences");
+    }
 
     private void drawGeofences(List<GeofenceObject> listOfGeofenceObjects) {
         for (GeofenceObject geofenceObject : listOfGeofenceObjects) {
@@ -135,11 +160,100 @@ public class MapManager implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        setInitialView();
         getRoute();
         getGeofences();
+        setInitialView();
     }
 
+    /**
+     * Builds and returns a GeofencingRequest. Specifies the list of geofences to be monitored.
+     * Also specifies how the geofence notifications are initially triggered.
+     */
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+        // is already inside that geofence.
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+
+        // Add the geofences to be monitored by geofencing service.
+        builder.addGeofences(listOfGeofences);
+
+        // Return a GeofencingRequest.
+        return builder.build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationServices.GeofencingApi.addGeofences(
+                mGoogleApiClient,
+                getGeofencingRequest(),
+                getGeofencePendingIntent()
+        ).setResultCallback(this);
+
+        startLocationUpdates();
+        Log.d("onConnected()", "--was just called--");
+    }
+
+    private void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+        Log.d("start loc updates", "---was just called---");
+    }
+
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    /**
+     * Gets a PendingIntent to send with the request to add or remove Geofences. Location Services
+     * issues the Intent inside this PendingIntent whenever a geofence transition occurs for the
+     * current list of geofences.
+     *
+     * @return A PendingIntent for the IntentService that handles geofence transitions.
+     */
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(mContext, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        return PendingIntent.getService(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(Status status) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateCamera(location);
+    }
+
+    /**
+     * AsynTask that fetches latitude and longitude from our REST API,
+     * converts them into LatLng objects, stores them in a list, and
+     * passes this list to drawRoute() for drawing.
+     */
     private class DownloadRouteTask extends AsyncTask<String, Void, JSONObject> {
 
         @Override
@@ -204,6 +318,7 @@ public class MapManager implements OnMapReadyCallback {
                 e.printStackTrace();
             }
 
+            List<LatLng> listOfRouteCoordinates = new ArrayList<>();
             for (int i = 0; i < resources.length(); i++) {
                 try {
                     JSONObject point = resources.getJSONObject(i);
@@ -211,21 +326,21 @@ public class MapManager implements OnMapReadyCallback {
                     String lng = point.getString("lng");
 
                     LatLng latLng = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
-                    mRouteCoordinates.add(latLng);
+                    listOfRouteCoordinates.add(latLng);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
 
 //            loadingToast.cancel();
-            drawRoute();
+            drawRoute(listOfRouteCoordinates);
         }
     }
 
     /**
      * AsyncTask that fetches latitudes, longitudes, and radii from our REST API,
      * then converts them into GeofenceObjects, which contain LatLng and radius properties.
-     * These GeofenceObjects are then stored into a member variable to be used for drawing.
+     * These GeofenceObjects are then passed to drawGeofences() for drawing.
      */
     private class DownloadGeofencesTask extends AsyncTask<String, Void, JSONObject> {
 
@@ -285,22 +400,38 @@ public class MapManager implements OnMapReadyCallback {
                 e.printStackTrace();
             }
 
-            List<GeofenceObject> listOfGeofenceObjects = new ArrayList<>();
+//            List<GeofenceObject> listOfGeofenceObjects = new ArrayList<>();
             for (int i = 0; i < resources.length(); i++) {
                 try {
                     JSONObject point = resources.getJSONObject(i);
+                    String id = point.getString("id");
                     String lat = point.getString("lat");
                     String lng = point.getString("lng");
                     String rad = point.getString("rad");
 
-                    listOfGeofenceObjects.add(new GeofenceObject(lat, lng, rad));
+//                    listOfGeofenceObjects.add(new GeofenceObject(lat, lng, rad));
+
+                    listOfGeofences.add(new Geofence.Builder()
+                            .setRequestId(id)
+                            .setCircularRegion(
+                                    Double.parseDouble(lat),
+                                    Double.parseDouble(lng),
+                                    Float.parseFloat(rad))
+                            .setExpirationDuration(GEOFENCE_LIFETIME)
+                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                            .build());
+
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
 
             loadingToast.cancel();
-            drawGeofences(listOfGeofenceObjects);
+
+            // don't need to actually draw geofence, since they should be invisible
+//            drawGeofences(listOfGeofenceObjects);
+            mGoogleApiClient.connect();
         }
     }
 }
